fa24a1fade33bcb2d269d13050786ba6b0fa3a3c
Lenz Weber <lenz.weber@mayflower.de>
2016-07-01 14:33:32 +0200

    parse ini into objects

--- renderer.js
+++ renderer.js
@@ -2,8 +2,66 @@
 let electron = require('electron');
 let fs = require('fs');
 
-electron.ipcRenderer.on('openFile', function(sender, fileName){
-   fs.readFile(fileName, 'utf-8', function(err, data) {
-       $('#foobar').val(data);
+electron.ipcRenderer.on('openFile', function (sender, fileName) {
+    fs.readFile(fileName, 'utf-8', function (err, data) {
+        let elements = [];
+        let lines = data.split("\n");
+        lines.forEach(function (line) {
+            let element = IniElement.getElement(line);
+            if (element) {
+                elements.push(element);
+            }
+        });
+        console.log(elements);
     });
 });
+
+/**
+ * @class
+ * @property {string} name
+ */
+class IniElement {
+    constructor(line) {
+        let matches = line.match(this.constructor.regex);
+        this.name = matches[1];
+    }
+
+    static getElement(line) {
+        console.log(line);
+        if (line.match(IniSection.regex)) {
+            return new IniSection(line);
+        }
+        if (line.match(IniComment.regex)) {
+            return new IniComment(line);
+        }
+        if (line.match(IniValue.regex)) {
+            return new IniValue(line);
+        }
+    }
+}
+
+class IniSection extends IniElement {
+    static get regex() {
+        return /^\s*\[(.*)\]\s*$/;
+    }
+}
+class IniComment extends IniElement {
+    static get regex() {
+        return /^;(.*)/;
+    }
+}
+/**
+ * @class
+ * @property {string} value
+ */
+class IniValue extends IniElement {
+    static get regex() {
+        return /^\s*(.*?)=(.*)\s*$/;
+    }
+
+    constructor(line) {
+        super(line);
+        let matches = line.match(this.constructor.regex);
+        this.value = matches[2];
+    }
+}
