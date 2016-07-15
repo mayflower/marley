b7dd43bdc96ce7cb2cb76dc260b5a3fb1f86a9bc
Maria Haubner <maria.haubner@mayflower.de>
2016-07-15 14:32:46 +0200

    confirmation before delete

--- js/remove-value.js
+++ js/remove-value.js
@@ -3,18 +3,8 @@ const $ = require('jquery');
 $(document).on('click', '.remove-value', function () {
     let $value = $(this).parents('li').first();
 
-    $value.remove();
-
+    let isConfirmed = confirm('Wollen Sie diesen Eintrag wirklich löschen?');
+    if (isConfirmed) {
+        $value.remove();
+    }
 });
-
-
-function testModal(text){
-    let $modal = $('<div class="modal fade" role="dialog"></div>');
-    let $modalDialog = $('<div class="modal-dialog"></div>').appendTo($modal);
-    let $modalHeader = $('<div class="modal-header">').appendTo($modalDialog);
-    let $modalBody = $('<div class="modal-body">').text(text).appendTo($modalDialog);
-    let $modalFooter = $('<div class="modal-footer">').appendTo($modalDialog);
-
-    $modal.on('hidden.bs.modal', ()=>{ $modal.remove(); } ).appendTo('body');
-    $modal.modal();
-}
