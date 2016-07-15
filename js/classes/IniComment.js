4da029ff93a6c9cb67d7062bc26fafec3068a682
Maria Haubner <maria.haubner@mayflower.de>
2016-07-15 13:48:48 +0200

    remove comments

--- js/classes/IniComment.js
+++ js/classes/IniComment.js
@@ -14,7 +14,10 @@ module.exports = class IniComment extends IniElement {
         let $element = $(`
         <li class="ini-comment"><div class="row">
             <div class="col-sm-1">;</div>
-            <div class="col-sm-11"><textarea class="form-control name" rows="1">${this.name}</textarea></div>
+            <div class="col-sm-11 flex-col">
+                <textarea class="form-control name" rows="1">${this.name}</textarea>
+                <button type="button" class="btn btn-default remove-value"><i class="glyphicon glyphicon-minus"></i></button>
+            </div>
         </div></li>`);
 
         let $name = $element.find('.name');
